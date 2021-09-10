22ae07020ccc29bc50b4f5bf2a6c3723f106f235
Kirill Gordievich <kirill.gordievich777@gmail.com>
2021-09-10 21:46:28 +0300

    updated task "2.1 Errors and Exceptions step 8"

--- Stepik/2_1_step_8.py
+++ Stepik/2_1_step_8.py
@@ -17,12 +17,12 @@ print(f'Initial list = {list_}')
 while True:
     try:
         list_.append(int(input("Please enter a number: ")))
-        break
     except NonPositiveError:
         print("Only positive numbers are allowed to append! Try again...")
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
-    finally:
+    else:
         print("The number was appended")
+        break  # leave the circle
 
 print(f'Updated list = {list_}')
